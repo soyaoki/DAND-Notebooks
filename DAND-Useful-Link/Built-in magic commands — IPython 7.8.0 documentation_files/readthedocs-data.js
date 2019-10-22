d819c2f5497a6015d579dbe22ff15fa7d5b44c44
Soya AOKI <45820850+soyaoki@users.noreply.github.com>
2019-10-22 17:46:30 +0900

    Add files via upload

---
+++ DAND-Useful-Link/Built-in magic commands — IPython 7.8.0 documentation_files/readthedocs-data.js
@@ -0,0 +1,19 @@
+var READTHEDOCS_DATA = {
+    project: "ipython",
+    version: "stable",
+    language: "en",
+    programming_language: "py",
+    subprojects: {},
+    canonical_url: "https://ipython.readthedocs.io/en/stable/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/docs/source/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "c233c25a",
+    ad_free: false,
+
+    global_analytics_code: 'UA-17997319-1',
+    user_analytics_code: 'UA-52617120-6'
+};
+
---
+++ DAND-Useful-Link/Using as a command line tool — nbconvert 5.6.1.dev0 documentation_files/readthedocs-data.js
@@ -0,0 +1,19 @@
+var READTHEDOCS_DATA = {
+    project: "nbconvert",
+    version: "latest",
+    language: "en",
+    programming_language: "py",
+    subprojects: {},
+    canonical_url: "https://nbconvert.readthedocs.io/en/latest/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/docs/source/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "cda5ec4b",
+    ad_free: false,
+
+    global_analytics_code: 'UA-17997319-1',
+    user_analytics_code: 'UA-52617120-6'
+};
+
